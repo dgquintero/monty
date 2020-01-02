0f5ac45f1ea2296aa450d7cc10d7dca0b502f5e1
dgquintero <dgquintero02@hotmail.com>
2020-01-02 20:41:36 +0000

    monty.h

--- monty.h
+++ monty.h
@@ -13,19 +13,16 @@ typedef struct stack_s
         struct stack_s *prev;
         struct stack_s *next;
 } stack_t;
-
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
  *
- * Description: doubly linked list node structure
+ * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct stack_s
+typedef struct instruction_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;

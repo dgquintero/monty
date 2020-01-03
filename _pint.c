3d126d2df287ee124eff18637e99b745ec7016a4
dgquintero <dgquintero02@hotmail.com>
2020-01-03 21:28:24 +0000

    added some files

--- _pint.c
+++ _pint.c
@@ -3,21 +3,20 @@
 #include <stdio.h>
 #include "monty.h"
 /**
- *  _pall - function that prints all the elements of a dlist
+ * _pint - function that prints the last element of a list
  * @h : const - list_t
  *
  *
  * Return: the number of notes.
  */
-void _pall(const stack_t *h)
+void _pint(const stack_t *h)
 {
 	size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
-	return (i);
+	printf("%d\n", h->n);
 }

721f14abed417748b6e0fd597b2abd7393b02f48
michelalejo <michelmolina65@gmail.com>
2020-01-03 18:44:12 +0000

    main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	char buffer[1024];
+	int one;
+
+	if (argc != 2)
+	{dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	one = open(argv[1], O_RDONLY);
+	if (one == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+}

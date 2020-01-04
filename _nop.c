80a36bf83cd447e15fb4c6562efa9ac0e9b526a1
michelalejo <michelmolina65@gmail.com>
2020-01-04 05:11:39 +0000

    add _nop.c

--- _nop.c
+++ _nop.c
@@ -3,11 +3,10 @@
 #include <stdio.h>
 #include "monty.h"
 /**
- *  _pall - function that prints all the elements of a dlist
- * @h : const - list_t
- *
- *
- * Return: the number of notes.
+ * nop - The opcode that doesn’t do anything.
+ * @h: The stack pointer..
+ * @Count: counter of the line
+ * Return: None.
  */
 void _nop(stack_t **h, unsigned int count)
 {
